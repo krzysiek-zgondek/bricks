89455e57d7f04d91c1d11ebf0c14a5a4267504e7
krzysiek zgondek <krzysiek.zgondek@gmail.com>
2020-01-31 18:27:09 +0100

    #More test for activity arguments

--- android-activity/src/test/java/com/source/bricks/activity/ActivityArgumentTest.kt
+++ android-activity/src/test/java/com/source/bricks/activity/ActivityArgumentTest.kt
@@ -11,6 +11,7 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.core.app.launchActivity
 import com.source.bricks.activity.argument.argument
 import com.source.bricks.activity.argument.arguments
+import com.source.bricks.activity.argument.set
 import com.source.bricks.intent.intentFor
 import com.source.bricks.test.addTestActivities
 import kotlinx.android.parcel.Parcelize
@@ -33,24 +34,36 @@ class DirectArgumentActivity : Activity() {
     data class Parcel(val value: Int, val text: String) : Parcelable
 
     var text by Arguments.TextParam
+    var text2 by Arguments.TextParam2
     val serial by Arguments.SerialParam
+    val serial2 by Arguments.SerialParam2
     val parcel by Arguments.ParcelParam
+    val parcel2 by Arguments.ParcelParam2
+    val notProvidedDefault by Arguments.NotProvidedDefaultParam
     val notProvided by Arguments.NotProvidedParam
 
     companion object {
         private object Arguments {
             val TextParam by argument(default = "")
+            val TextParam2 by argument(default = "")
             val SerialParam by argument<Serial>()
+            val SerialParam2 by argument<Serial>()
             val ParcelParam by argument<Parcel>()
+            val ParcelParam2 by argument<Parcel>()
+            val NotProvidedDefaultParam by argument(default = "default")
             val NotProvidedParam by argument<Parcel>()
         }
 
         fun getIntent(context: Context): Intent {
             return context.intentFor<DirectArgumentActivity> {
+                set(Arguments.TextParam, "test")
+                set(Arguments.SerialParam, Serial(1, "test"))
+                set(Arguments.ParcelParam, Parcel(1, "test"))
+
                 arguments {
-                    Arguments.TextParam.set("test")
-                    Arguments.SerialParam.set(Serial(1, "test"))
-                    Arguments.ParcelParam.set(Parcel(1, "test"))
+                    Arguments.TextParam2.set("test")
+                    Arguments.SerialParam2.set(Serial(1, "test"))
+                    Arguments.ParcelParam2.set(Parcel(1, "test"))
                 }
             }
         }
@@ -77,9 +90,13 @@ class ActivityArgumentTest {
     fun `provide arguments and check if they were obtained properly`() {
         scenario.onActivity { activity ->
             Assert.assertEquals("test", activity.text)
+            Assert.assertEquals("test", activity.text2)
             Assert.assertNotEquals("test2", activity.text)
+            Assert.assertNotEquals("test2", activity.text2)
             Assert.assertEquals(DirectArgumentActivity.Serial(1, "test"), activity.serial)
+            Assert.assertEquals(DirectArgumentActivity.Serial(1, "test"), activity.serial2)
             Assert.assertEquals(DirectArgumentActivity.Parcel(1, "test"), activity.parcel)
+            Assert.assertEquals(DirectArgumentActivity.Parcel(1, "test"), activity.parcel2)
         }
     }
 
@@ -88,6 +105,13 @@ class ActivityArgumentTest {
         scenario.onActivity { activity ->
             val provided = activity.notProvided
             provided.text.hashCode()
+        }
+    }
+
+    @Test
+    fun `reading nonNull arguments with default value should return default if not provided`() {
+        scenario.onActivity { activity ->
+            Assert.assertEquals("default", activity.notProvidedDefault)
         }
     }
 
