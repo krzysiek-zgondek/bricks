6124e0d6017a449597b553ff7f7aff4cf586c932
krzysiek zgondek <krzysiek.zgondek@gmail.com>
2020-01-31 18:12:17 +0100

    #added method to manually set intent's argument instead of using arguments scope

--- android-activity/src/main/java/com/source/bricks/activity/argument/ArgumentScope.kt
+++ android-activity/src/main/java/com/source/bricks/activity/argument/ArgumentScope.kt
@@ -2,14 +2,16 @@ package com.source.bricks.activity.argument
 
 import android.content.Intent
 
+
+/*Future notes
+* when inline classes become stable make [ActivityArgumentScope] class inline as well
+* */
+
+
 /**
  * This scope enables setting arguments for activity's intent with simple [set] method
  * */
 class ActivityArgumentScope(val intent: Intent) {
-    /*Future notes
-    * when inline classes become stable make this class inline as well
-    * */
-
     /**
      * Saves value into intent using provided [ActivityArgument] description
      * */
@@ -23,4 +25,12 @@ class ActivityArgumentScope(val intent: Intent) {
  * */
 inline fun Intent.arguments(body: ActivityArgumentScope.() -> Unit): Intent {
     return ActivityArgumentScope(this).apply(body).intent
+}
+
+/**
+ * Sets argument's value using intent directly
+ * */
+inline fun <reified Type> Intent.set(argument: ActivityArgument<Type>, value: Type): Intent {
+    argument.encoder(this, argument.name, value)
+    return this
 }
