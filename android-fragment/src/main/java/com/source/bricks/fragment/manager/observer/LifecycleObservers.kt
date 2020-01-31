2306deabd4865c6ea25fa4a2aa34a120640265af
krzysiek zgondek <krzysiek.zgondek@gmail.com>
2020-01-31 22:37:51 +0100

    #added onviewdestroyed fragment lifecycle observer

--- android-fragment/src/main/java/com/source/bricks/fragment/manager/observer/LifecycleObservers.kt
+++ android-fragment/src/main/java/com/source/bricks/fragment/manager/observer/LifecycleObservers.kt
@@ -1,9 +1,8 @@
 package com.source.bricks.fragment.manager.observer
 
-import android.os.Bundle
-import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+
 
 inline fun <reified T : Fragment> FragmentManager.registerActiveObserver(
     tag: String? = T::class.java.name, recursive: Boolean = false, crossinline receiver: (T) -> Unit
@@ -31,6 +30,23 @@ inline fun <reified T : Fragment> FragmentManager.registerInActiveObserver(
                 receiver(current)
                 fm.unregisterFragmentLifecycleCallbacks(this)
             }
+        }
+    }
+
+    registerFragmentLifecycleCallbacks(callbacks, recursive)
+}
+
+inline fun <reified T : Fragment> FragmentManager.registerDestroyObserver(
+    tag: String? = T::class.java.name, recursive: Boolean = false, crossinline receiver: (T) -> Unit
+) {
+    val callbacks = object : FragmentManager.FragmentLifecycleCallbacks() {
+        override fun onFragmentViewDestroyed(fm: FragmentManager, current: Fragment) {
+            super.onFragmentViewDestroyed(fm, current)
+            if (current is T && current.tag == tag) {
+                receiver(current)
+                fm.unregisterFragmentLifecycleCallbacks(this)
+            }
+
         }
     }
 
